89d4608456939b2965a1f957ebe2c76f40ce34fb
Luc Blassel <luc.blassel@gmail.com>
2023-09-20 11:43:06 +0200

    Adding styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/*.html"],
+  safelist: [
+    {
+      pattern: /text-.+-(|500|700)/,
+      variants: ["hover"],
+    },
+  ],
   theme: {
     extend: {},
   },
